283afb3a4976245b862e2a00ca5b88dd5fe4c9de
radharukminiY <rukku558@gmail.com>
2021-08-01 02:01:33 +0530

    commit 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import StockList from './stockList';
+import UserList from './userList';
+import Hoc from './Hoc';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StocksData = [
+  {
+      id: 1,
+      name: 'Apple'
+
+  },
+  {
+      id: 2,
+      name: 'Microsoft'
+  },
+  {
+      id: 3,
+      name: 'France24'
+  }
+];
+const UsersData = [
+  {
+      id: 1,
+      name: 'Alexander'
+
+  },
+  {
+      id: 2,
+      name: 'Esabella'
+  },
+  {
+      id: 3,
+      name: 'Elenora'
+  }
+];
+
+const Stocks = Hoc(StockList,StocksData);
+const User = Hoc(UserList,UsersData);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       <Stocks/>
+       <User/>
+      </div>
+    )
+  }
 }
 
 export default App;
